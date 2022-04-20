1041bfee2dea89b56a42d207cedc3f8a91bee5b2
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-04-20 15:57:09 -0500

    Update three files

--- comparisons.rb
+++ comparisons.rb
@@ -22,24 +22,31 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 # this should print: "Is numberTeachers greater than numberStudents?" false
 
 # YOU DO: print the result of the comparison: is number_teachers less than number_students?
+puts "Is number_teachers less than number_students?", number_teachers < number_students
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_teachers equal to string_teachers?
+puts "Is number_teachers equal to string_teachers?", number_teachers == string_teachers
 # this should print: false
 
-# YOU DO: print the result of the comparison: is number_teachers not equal to number_students?
+# YOU DO: print the result of the comparison: is number_teachers not equal to number_stdents?
+puts "Is number_teachers not equal to number_students?", number_teachers != number_students
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 20?
+puts "Is number_students greater than or equal to 20?", number_students >= 20
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 21?
+puts "Is number_students greater than or equal to 21?", number_students >= 21
 # this should print: false
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 20?
+puts "Is number_sutdents less than or equal to 20?", number_students <= 20
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 21?
+puts "Is number_students less than or equal to 21?", number_students <= 21
 # this should print: true
 
 
@@ -53,21 +60,21 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 # Make sure YOU can explain it that way!
 
 puts 4 < 9
-#YOU DO: Explain.
+#YOU DO: Prints the result of the comparison: is 4 less than 9? This should print: true.
 
 books = 3
 puts 4 < books
-# YOU DO: Explain.
+# YOU DO: Prints the result of the comparison: is 4 less than books? Books is declared as 3, so this should print to false, since 4 is notless than 3.
 
 friends = 6
 siblings = 2
 puts friends > siblings
-# YOU DO: Explain.
+# YOU DO: Prints the result of the comparison: is friends greater than siblings? Friends is delcared as 6 and siblings 2, so this should print to true, since 6 is greater than 2.
 
 attendees = 9;
 meals = 8;
 puts attendees != meals
-# YOU DO: Explain.
+# YOU DO: Prints the results of the comparison: is attendees not equal to meals? attendees is declared as 9 and meals 8, so this should print to true, since 9 is not equal to 8.
 
 
 #-------------------
@@ -90,15 +97,16 @@ age = 1
 
 # YOU DO:
 # Determine if the dog loves to play and loves treats
-
+loves_to_play && loves_treats
 
 # Determine if the dog loves to play and loves the dog park
-
+loves_to_play && loves_dog_park
 
 # Determine if the dog loves to play or loves the dog park
-
+loves_to_play || loves_dog_park
 
 # Determine if the dog loves to play and is a puppy
+loves_to_play && age < 2
 
 # What did your final line of code evaluate to? Why do you think that is? Explain.
-# ANSWER:
+# ANSWER: The code evaluates to true because I defined being a puppy as being younger than 2. Since the dog loves to play and is younger than 2, both are true and the code evaluates to true.
--- decision_making.rb
+++ decision_making.rb
@@ -35,9 +35,27 @@ end
 
 # Questions
 # 1. In English, using technical vocabulary, describe what is happening between lines 12 and 16.
+# The code checks which door you opened by looking at the value assigned to door_choice and assigns a string value to bear_clothing based on the choice.
+# If door_choice is 1, then bear_clothing is "hat," and otherwise, bear_clothing is "scarf."
+
 # 2. What variable has a new value assigned to it after the first if statement executes?
+# bear_clothing is assigned the value "hat".
+
 # 3. If you changed the variable door_choice to equal 3, what would the bear_clothing value be?
+# bear_clothing would be "scarf".
+
 # 4. In English, using technical vocabulary, describe what is happening between lines lines 25 and 33.
+# The code checks which option you chose for how to interact (or not) with the bear by looking at the value assigned to bear_choice.
+# If bear_choice is assigned a value of 1, then you offer them your hat or scarf (depending on the value assigned to bear_clothing) and the bear shows you a secret passage out.
+# If bear_choice is 2, then you make the bear cry.
+# If bear_choice is 3, then you run away but run into a room full of snakes.
+# If bear_choice is any number that is not 1, 2, or 3, then you stay with the bear and become its best friend.
+
 # 5. If you changed the variable bear_choice to equal 3, what will be the final outcome be?
+# If bear_choice is 3, then you run away from the bear but run into a room full of snakes.
+
 # 6. If you changed the variable door_choice to equal 1, and the variable bear_choice to equal 2, what will be the final outcome be?
+# If door_choice is 1 and bear_choice is 2, then you tell the bear the hat is too small and make it cry.
+
 # 7. What is your favorite ending?
+# My favorite ending is to have the bear show me the secret passage. I do not mess with bears. 
